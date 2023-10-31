cf3ff5be4f8528bce2e7e9c7f671cbda1d19d8e8
ririo08 <ririo08@users.noreply.github.com>
2023-10-31 18:28:56 +0000

    deploy: d5677b7c69a86d9d2beb1ccae616bdb9cded42cf

---
+++ _nuxt/index.5732b62d.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./entry.ea470b50.js";const n={};function r(t,c){return"init page(wip)"}const i=e(n,[["render",r]]);export{i as default};
--- pages/index.vue
+++
@@ -1,5 +0,0 @@
-<script lang="ts" setup></script>
-
-<template>init page(wip)</template>
-
-<style lang="scss"></style>
