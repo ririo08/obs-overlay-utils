5c1c122148a86a33b95fa2a3a06bcfc448721074
ririo08 <ririo08@users.noreply.github.com>
2025-01-02 12:44:38 +0000

    deploy: 364ac6a702730528c63bfefe739f12b50095adda

---
+++ _nuxt/lUM7qruO.js
@@ -0,0 +1,1 @@
+import{aq as t,e as a,o as r,c as s,I as l}from"./DopW-7J7.js";const n=()=>t("color-mode").value,m=a({__name:"overlay",setup(c){const e=n();return e.value="light",(o,u)=>(r(),s("section",null,[l(o.$slots,"default")]))}});export{m as default};
